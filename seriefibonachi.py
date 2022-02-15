108ad63d82a2ceef8c20c3a886fec378395a54c2
AngieMarcela19 <amarcelapertuz19@gmail.com>
2022-02-15 10:46:12 -0500

    cambio en el comentario

--- seriefibonachi.py
+++ seriefibonachi.py
@@ -1,4 +1,4 @@
-N= int(input('ingrese un numero: '))
+N= int(input('ingrese un numero: ')) #indica los primeros n Primeros numeros que desea ver
 a= 0
 b=1 
 for i in range(1,N+1):
@@ -6,9 +6,3 @@ for i in range(1,N+1):
     c=a+b #suma 
     a=b
     b=c
- 
-
-    
-
-
-
